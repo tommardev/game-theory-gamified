3f3b4e91681e22aa4c1d45f6b39618d85461415e
tommardev <tommardev@gmail.com>
2026-07-15 01:43:57 +0300

    ci: add GitHub Actions workflow to deploy Next.js to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGithubActions ? "/game-theory-gamified" : "",
+};
 
 export default nextConfig;
